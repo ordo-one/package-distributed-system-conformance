4f55a94c47d5278c7d35b612e216f874a4f108c3
ser-0xff <122270051+ser-0xff@users.noreply.github.com>
2023-11-20 11:52:02 +0300

    fix(patch): [sc-455] Initial commit.

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import class Foundation.ProcessInfo
+import PackageDescription
+
+let internalDependencies: [String: Range<Version>] = [
+    "package-concurrency-helpers": .upToNextMajor(from: "2.0.0"),
+    "package-frostflake": .upToNextMajor(from: "3.1.0"),
+]
+
+func makeDependencies() -> [Package.Dependency] {
+    let localPath = ProcessInfo.processInfo.environment["LOCAL_PACKAGES_DIR"]
+    if let localPath {
+        return internalDependencies.map { .package(name: "\($0.key)", path: "\(localPath)/\($0.key)") }
+    } else {
+        return internalDependencies.map { .package(url: "https://github.com/ordo-one/\($0.key)", $0.value) }
+    }
+}
+
+let package = Package(
+    name: "package-distributed-system-conformance",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16),
+    ],
+    products: [
+        .library(
+            name: "DistributedSystemConformance",
+            type: .dynamic,
+            targets: ["DistributedSystemConformance"]
+        ),
+    ],
+    dependencies: makeDependencies(),
+    targets: [
+        .target(
+            name: "DistributedSystemConformance",
+            dependencies: [
+                .product(name: "Frostflake", package: "package-frostflake"),
+                .product(name: "Helpers", package: "package-concurrency-helpers"),
+            ]
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,2 @@
+# package-distributed-system-conformance
+
